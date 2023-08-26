fba18f5dc4234e7346f74aa7929498025c40c423
Matthew Connley <matt@mattconnley.com>
2023-08-26 11:21:09 -0500

    add hsts
    Signed-off-by: Matthew Connley <matt@mattconnley.com>

--- Program.cs
+++ Program.cs
@@ -26,7 +26,13 @@ try
     builder.Services.AddScoped<IRequestIPFinder, RequestIPFinder>();
     builder.Services.AddScoped<MyLogger, MyLogger>();
     builder.Logging.ClearProviders();
-    //builder.Host.UseNLog();
+    
+    builder.Services.AddHsts(options =>
+    {
+        options.Preload = true;
+        options.IncludeSubDomains = true;
+        options.MaxAge = TimeSpan.FromDays(90);
+    });
 
     builder.Services.AddW3CLogging(logging =>
     {
