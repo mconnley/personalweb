11b8f5271e6f4f842474b4cb35f9fbcb1dfdb416
Matthew Connley <matt@mattconnley.com>
2024-11-27 11:23:48 -0600

    Address ASP0019
    Signed-off-by: Matthew Connley <matt@mattconnley.com>

--- Program.cs
+++ Program.cs
@@ -88,11 +88,11 @@ try
 
     app.Use(async (context, next) =>
     {
-        context.Response.Headers.Add("X-Frame-Options", "DENY");
-        context.Response.Headers.Add("Content-Security-Policy", csp);
-        context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-        context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
-        context.Response.Headers.Add("Strict-Transport-Security", "max-age=15768000; includeSubDomains; preload");
+        context.Response.Headers.Append("X-Frame-Options", "DENY");
+        context.Response.Headers.Append("Content-Security-Policy", csp);
+        context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+        context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
+        context.Response.Headers.Append("Strict-Transport-Security", "max-age=15768000; includeSubDomains; preload");
         await next();
     });
 
